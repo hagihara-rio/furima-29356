db232956f31877d4f62453416cd5193f56f4527d
hagihara-rio <rio1210h@icloud.com>
2020-09-14 15:01:17 +0900

    item モデル マイグレート

---
+++ db/migrate/20200914031743_create_items.rb
@@ -0,0 +1,20 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+
+      t.string     :itemname,       null:false, limit: 40
+      t.text       :detail,         null:false, limit: 1000
+      t.integer    :category,       null:false
+      t.integer    :condition,      null:false
+      t.integer    :shipping_fee,   null:false
+      t.integer    :shipping_area,  null:false
+      t.integer    :shipping_days,  null:false
+      t.integer    :price,          null:false
+      t.references :user,           null:false
+
+      t.timestamps
+
+    end
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_102848) do
+ActiveRecord::Schema.define(version: 2020_09_14_031743) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "itemname", limit: 40, null: false
+    t.text "detail", null: false
+    t.integer "category", null: false
+    t.integer "condition", null: false
+    t.integer "shipping_fee", null: false
+    t.integer "shipping_area", null: false
+    t.integer "shipping_days", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
