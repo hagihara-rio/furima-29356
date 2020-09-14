b442c626adabfa053f3bcb5c8649e8dedff629ea
hagihara-rio <rio1210h@icloud.com>
2020-09-14 18:36:41 +0900

    ログインの有無でのページ移動（authenticate_user!）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,15 @@
 class ItemsController < ApplicationController
 
+  before_action :authenticate_user!, except:[:index]
+
   def index
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
   end
 
 end
