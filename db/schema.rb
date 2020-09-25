913b9b5c777b20466e85a52a2a56914a1d0f371c
hagihara-rio <rio1210h@icloud.com>
2020-09-26 01:43:29 +0900

    address入力フォーム実装

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_09_25_095500) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postal_code", null: false
-    t.string "prefectures", null: false
+    t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "address", null: false
     t.string "building_name"
